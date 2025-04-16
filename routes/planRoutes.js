6c3be78c367c14ee48808e17e36f4e10cfe5399f
lena mukhtar <lena.mukhtar@gmail.com>
2025-04-15 23:53:45 -0400

    addding changes to calendar and workout and updated the css

--- routes/planRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPlan, getPlans } = require('../controllers/planController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, createPlan);
-router.get('/', protect, getPlans);
-
-module.exports = router;

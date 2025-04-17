61a386976811697ae002f079924b139a02eb41da
Nebil ferej <nabilrooney123@gmail.com>
2025-04-16 22:28:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
 const userRoutes = require('./routes/userRoutes');
-const planRoutes = require('./routes/planRoutes');
-const trackingRoutes = require('./routes/trackingRoutes');
 
 dotenv.config();
 connectDB();
@@ -13,8 +11,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-app.use('/api/plans', planRoutes);
-app.use('/api/trackings', trackingRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

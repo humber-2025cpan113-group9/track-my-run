9e237e3f5c406e3b70add97270d8f6693c11653f
lena mukhtar <lena.mukhtar@gmail.com>
2025-04-16 22:20:55 -0400

    made changes to the code nd layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,26 @@
+//validate the calendar 
+document.getElementById("calendar-btn").addEventListener("click", function validateForm(){
+    
+    const day = document.querySelector('.slider').dataset.day;
+    const workout = document.getElementById(`${day}-distance`).value;
+    const distance = document.getElementById(`${day}-distance`).value;
+    
+    console.log(day);
+    console.log(workout);
+    console.log(distance);
+
+    if (day === "" || workout === "" || distance === "") {
+        alert("Please fill in all required fields.");
+        return false;
+    }
+
+    return true;
+})
+
+
+//user friendly slider 
 document.addEventListener("DOMContentLoaded", () => {
     const mileageSliders = document.querySelectorAll(".milage");
-    const timeSliders = document.querySelectorAll(".time");
   
     mileageSliders.forEach(slider => {
       const day = slider.dataset.day;
@@ -11,12 +31,5 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   
-    timeSliders.forEach(slider => {
-      const day = slider.dataset.day;
-      const display = document.getElementById(`${day}-time`);
   
-      slider.addEventListener("input", () => {
-        display.textContent = `${slider.value} min`;
-      });
-    });
   });

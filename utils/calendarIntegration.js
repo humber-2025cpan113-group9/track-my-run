991e6ca6b9dfcef5b2b78148f4352055c78dbb64
CL <leungc22@gmail.com>
2025-04-17 23:14:32 -0400

    website redo

--- utils/calendarIntegration.js
+++
@@ -1,6 +0,0 @@
-function addToCalendar(userEmail, eventTitle, date) {
-    console.log(`[Calendar] Event "${eventTitle}" scheduled for ${userEmail} on ${date}`);
-  }
-  
-  module.exports = { addToCalendar };
-  
